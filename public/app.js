d63cbc604f6b613dc885dfc0324fc77f780e35d3
Mohsensho <shojaeifar.mo@yahoo.com>
2019-05-29 12:31:54 -0700

    app.js updated

--- public/app.js
+++ public/app.js
@@ -1,12 +1,12 @@
 // Grab the articles as a json
 $.getJSON("/articles", function(data) {
   // For each one
-  for (var i = 0; i < data.length; i++) {
+  for (var i = 1; i < data.length; i++) {
     // Display the apropos information on the page
     $("#articles").append(`
     <div class="card">
       <div class="card-header">
-      #${i+1}
+      #${i}
       </div>
       <div class="card-body">
         <blockquote class="blockquote mb-0">
